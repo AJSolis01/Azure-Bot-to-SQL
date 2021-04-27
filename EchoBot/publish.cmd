d501b6d25ca35fa83c5993a6f8705e054379ad78
AJSolis01 <57882033+AJSolis01@users.noreply.github.com>
2021-04-27 17:22:04 -0400

    Delete EchoBot directory

--- EchoBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\cloudcomputebot-Web-Deploy.pubxml -p:Password=hiTuAhQDqT81dSrkQRoq0xGav4BresKxfneFHF8LXpHhKoGNEQaHGi5rRuW9
-
