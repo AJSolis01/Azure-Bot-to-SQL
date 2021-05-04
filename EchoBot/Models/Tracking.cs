99373f26a700953b7fa1efd22a2c5d5a0be26fc0
AJSolis01 <57882033+AJSolis01@users.noreply.github.com>
2021-05-04 17:37:57 -0400

    EchoBot

---
+++ EchoBot/Models/Tracking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bot.Models1
+{
+    public partial class Tracking
+    {
+        public string TrackingId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
